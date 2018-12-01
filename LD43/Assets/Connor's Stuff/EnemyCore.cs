d7dede706b8eb62750d7fdea943bf3016de005b8
connornagel586 <connornagel@u.boisestate.edu>
2018-11-30 23:26:50 -0800

    Started scripts
    Created an enemy parent class for all enemies to use. Added a goat class and player class.

---
+++ LD43/Assets/Connor's Stuff/EnemyCore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyCore : MonoBehaviour {
+    
+    //Holds enemy health and other important values accosiated with all enemies
+    private int health;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected int DecrementHealth() {this.health--; return this.health;}
+
+    protected int IncrementHealth() {this.health++; return this.health;}
+
+    protected int GetHealth() { return this.health; }
+
+    protected void SetHealth(int life) { health = life; }
+
+    protected abstract void OnHit();
+
+    protected abstract void OnKill();
+}
