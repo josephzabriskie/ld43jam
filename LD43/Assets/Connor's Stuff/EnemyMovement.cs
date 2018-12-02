c203dbb378729689952019690febefad34c5495c
connornagel586 <connornagel@u.boisestate.edu>
2018-12-02 13:53:18 -0800

    Added static enemy movement class and Goblin script

---
+++ LD43/Assets/Connor's Stuff/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EnemyMovement {
+
+	// Use this for initialization
+
+    public static void moveTowardsTarget(GameObject player, Rigidbody2D enemy, float speed)
+    {
+        Vector2 direction = (Vector2)player.transform.position - enemy.position;
+
+        direction.Normalize();
+        Vector3 difference = player.transform.position - enemy.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        enemy.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90);
+
+        enemy.velocity = direction * speed;
+    }
+}
