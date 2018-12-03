bdf0d5d38ba755980413719b8d57c8679aa340b5
Wumbomancer <39326014+Wumbomancer@users.noreply.github.com>
2018-12-02 20:58:48 -0800

    Sounds

---
+++ LD43/Assets/AudioManager.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static bool updateSound;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.volume = s.volume;
+            s.source.outputAudioMixerGroup = s.MainAudio;
+        }
+    }
+
+    public void Update()
+    {
+        if (updateSound)
+        {
+            UpdateSound();
+            updateSound = false;
+
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " is not found");
+            return;
+        }
+
+        if (s.source.isPlaying)
+        {
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " is not found");
+            return;
+        }
+
+        s.source.Stop();
+    }
+
+    public void UpdateSound()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.volume = s.volume;
+        }
+    }
+}
