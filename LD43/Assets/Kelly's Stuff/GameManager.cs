9f47426471cb927743c0a964bbd56c74a4fe459c
Wumbomancer <39326014+Wumbomancer@users.noreply.github.com>
2018-12-02 20:03:59 -0800

    Functioning Main

---
+++ LD43/Assets/Kelly's Stuff/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public Animator FadeControl;
+
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(FindObjectOfType<Canvas>().gameObject);
+    }
+    public void FadeToBlack()
+    {
+        
+        FadeControl.SetBool("FadeTime", true);
+        
+        
+    }
+
+    public void ResetFade()
+    {
+        FadeControl.SetBool("FadeTime", false);
+    }
+}
